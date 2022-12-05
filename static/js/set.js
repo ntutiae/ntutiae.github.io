d779b72e447b198f5f06b2cc419b1a84b1f5dce9
y060 <060yoyo@gmail.com>
2022-12-05 13:30:41 +0800

    improvement
    - 各頁面標題
    - 新增「科系特色」進navbar
    - 修改首頁教學特色html
    - 師資介紹footer
    - 活動內頁留言css
    - 學習心得影片彈出視窗RWD
    - 常見問題arrow動畫

--- static/js/set.js
+++ static/js/set.js
@@ -1,9 +1,20 @@
 
+function Footer(){
+    let wh = window.screen.height;
+    let footer = document.getElementsByClassName("footer")[0]
+    let ft = footer.scrollHeight, fh = footer.height, fm = footer.style.marginTop;
+
+    if( (ft+fh) < wh ){
+        let n = (wh-ft-fh);
+        fm = (fm + n) + "px";
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     // navbar
     document.getElementsByClassName("active")[0].parentNode.parentNode.previousSibling.previousSibling.style.opacity = "1";
-    
+})
 
-    // footer
-    // console.log(document.getElementById("Footer").scrollTop)
+window.addEventListener("resize", () => {
+    Footer();
 })
