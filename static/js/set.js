a87b79be9ba753d7c9794d7da983229c2032ac13
y060 <060yoyo@gmail.com>
2022-12-12 03:13:24 +0800

    improvement
    - navbar 手機版
    - 首頁動畫
    - 活動總覽hover動畫
    - 學習心得影片 手機版彈出視窗

--- static/js/set.js
+++ static/js/set.js
@@ -1,18 +1,26 @@
 
 function Footer(){
     let wh = window.screen.height;
-    let footer = document.getElementsByClassName("footer")[0]
-    let ft = footer.scrollHeight, fh = footer.height, fm = footer.style.marginTop;
+    let footer = document.getElementsByTagName("footer")[0];
+    console.log(footer)
+    let ft = footer.offsetTop, fh = footer.height, fm = footer.style.marginTop;
+    console.log(wh)
+    console.log(ft)
 
     if( (ft+fh) < wh ){
-        let n = (wh-ft-fh);
-        fm = (fm + n) + "px";
+        footer.style.marginTop = (fm + (wh-ft-fh)) + "px";
     }
 }
 
+
 document.addEventListener("DOMContentLoaded", () => {
+    AOS.init();
+
     // navbar
     document.getElementsByClassName("active")[0].parentNode.parentNode.previousSibling.previousSibling.style.opacity = "1";
+
+    //footer
+    Footer();
 })
 
 window.addEventListener("resize", () => {
