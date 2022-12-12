465bced029ac9bad7e769dd88e78f7c0af1b9b52
y060 <060yoyo@gmail.com>
2022-12-12 16:54:45 +0800

    首頁RWD樣式 & footer置底

--- static/js/set.js
+++ static/js/set.js
@@ -1,28 +1,7 @@
 
-function Footer(){
-    let wh = window.screen.height;
-    let footer = document.getElementsByTagName("footer")[0];
-    console.log(footer)
-    let ft = footer.offsetTop, fh = footer.height, fm = footer.style.marginTop;
-    console.log(wh)
-    console.log(ft)
-
-    if( (ft+fh) < wh ){
-        footer.style.marginTop = (fm + (wh-ft-fh)) + "px";
-    }
-}
-
-
 document.addEventListener("DOMContentLoaded", () => {
     AOS.init();
 
     // navbar
     document.getElementsByClassName("active")[0].parentNode.parentNode.previousSibling.previousSibling.style.opacity = "1";
-
-    //footer
-    Footer();
-})
-
-window.addEventListener("resize", () => {
-    Footer();
 })
